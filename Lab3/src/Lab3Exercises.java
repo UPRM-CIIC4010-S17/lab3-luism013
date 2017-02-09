570ef39223418dfd2312ca2d1809d656e7803ac6
Luis Rivera <Luis Rivera@DESKTOP-V3BO3O5>
2017-02-09 09:55:54 -0400

    Changes:
    Added a polygon which turned out to be an arrow.
    Added a polygon which turned out to be a star.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Luis Rivera");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 500);
-		myFrame.setSize(200,200);
+		myFrame.setSize(300, 300);
 		
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
