fa4c8b1edecc3cfb9919223a7539a0c1788a0622
4015c16 <4015c16@granada.ece.uprm.edu>
2017-11-15 16:09:15 -0400

    uncommented the default close operation so it works properly.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,11 +4,11 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Luis Rivera");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 500);
-		myFrame.setSize(400, 250);
+		myFrame.setLocation(900, 450);
+		myFrame.setSize(200, 200);
 		
-		MyPanelClass myPanel = new MyPanelClass();
-        myFrame.getContentPane().add(myPanel);
+//		MyPanelClass myPanel = new MyPanelClass();
+//        myFrame.getContentPane().add(myPanel);
 
 		myFrame.setVisible(true);
 		
